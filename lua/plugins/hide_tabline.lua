1081a0822ab640e2c69b640d9e6ae41e50d02e90
John Wilger <john@johnwilger.com>
2024-06-03 15:49:42 -0700

    Misc updates

---
+++ lua/plugins/hide_tabline.lua
@@ -0,0 +1,11 @@
+return {
+  "AstroNvim/astrocore",
+  ---@param opts AstroCoreOpts
+  opts = {
+    options = {
+      opt = {
+        showtabline = 0,
+      },
+    },
+  },
+}
--- lua/polish.lua
+++ lua/polish.lua
@@ -3,8 +3,40 @@
 -- fit in the normal config locations above can go here
 
 local function cmd(command) return table.concat { "<Cmd>", command, "<CR>" } end
+local function set(mode, key, command) vim.keymap.set(mode, key, cmd(command)) end
 
-vim.keymap.set("n", "<C-w>z", cmd "WindowsMaximize")
-vim.keymap.set("n", "<C-w>_", cmd "WindowsMaximizeVertically")
-vim.keymap.set("n", "<C-w>|", cmd "WindowsMaximizeHorizontally")
-vim.keymap.set("n", "<C-w>=", cmd "WindowsEqualize")
+set("n", "<Leader>;", "b#")
+set("n", "<Leader>a", "AerialToggle")
+
+vim.o.scrolloff = 20
+vim.o.sidescrolloff = 20
+vim.o.relativenumber = false
+
+-- Autocommand group for adjusting Telescope window settings
+vim.api.nvim_create_augroup("TelescopeCustomizations", { clear = true })
+
+-- Adjust settings when entering a Telescope window
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "TelescopePrompt,TelescopeResults",
+  group = "TelescopeCustomizations",
+  callback = function()
+    vim.wo.scrolloff = 0
+    vim.wo.sidescrolloff = 0
+  end,
+})
+
+-- Reset settings when leaving a Telescope window
+vim.api.nvim_create_autocmd("WinLeave", {
+  pattern = "*",
+  group = "TelescopeCustomizations",
+  callback = function()
+    if not vim.bo.filetype:match "Telescope" then
+      vim.wo.scrolloff = 20
+      vim.wo.sidescrolloff = 20
+    end
+  end,
+})
+
+vim.diagnostic.config {
+  virtual_text = false,
+}
